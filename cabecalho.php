5f9c1c5d367cfe39dca0c8b974965051f7aed6bb
Bruno Barreto Murad <bbmurad.dev@gmail.com>
2025-04-28 00:53:28 -0300

    Inicio de programação do Site

---
+++ cabecalho.php
@@ -0,0 +1,38 @@
+<div id="cabecalho-superior">
+    <nav id="menu-superior">
+        <ul>
+            <li> <a href="#">Minha Conta</a></li>
+            <li> <a href="#">Meu Carrinho</a></li>
+            <li> <a href="#">Logar</a></li>
+            <li> <a href="#">Cadastrar</a></li>
+        </ul>
+    </nav>
+
+</div>
+<div id="cabecalho-meio">
+    <h1>HORTIFRUTI ONLINE - O melhor site de compras de frutas e legumes do Rio !</h1>
+    <p>Nenhum item no seu carrinho de compras
+    </p>
+    <section>
+        <form action="">
+            <label>
+                <span> buscar </span>
+                <input type="search" name="pesquisa" id="pesquisa">
+                <input type="image" src="imagens/lupa24.png">
+            </label>
+        </form>
+    </section>
+</div>
+<div id="cabecalho-inferior">
+    <nav id="menu-principal">
+        <ul>
+            <li> <a href="#">Home</a> </li>
+            <li> <a href="#">Produtos</a> </li>
+            <li> <a href="#">Fale Conosco</a> </li>
+            <li> <a href="#">Quem Somos</a> </li>
+        </ul>
+
+    </nav>
+
+
+</div>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/css.css">
+    <title>Hortifruti On Line</title>
+</head>
+<body>
+    <div id="principal">
+        <section id="cabecalho">
+            <?php include_once ('cabecalho.php'); ?>
+        </section> <!-- fim da section  cabeçalho-->
+        <section id ="principal">
+        
+        </section> <!-- fim da section principal -->
+        <footer>
+        
+        </footer> <!-- fim do rodapé -->
+    </div> <!-- fim da div principal -->
+</body>
+</html>
