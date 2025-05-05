cb0ca1a598233713dde307d8909e6f43dda5ea31
Bruno Barreto Murad <bbmurad.dev@gmail.com>
2025-05-05 02:49:57 -0300

    05052025 0249hs
    Criando as classes de conexao e manipulação de dados com php para o banco de dados.
    Assistido ate a aula 26.

---
+++ classes/conexaoMySQL.php
@@ -0,0 +1,37 @@
+<?php
+    class conexaoMySQL {
+        protected $servidor;
+        protected $usuario;
+        protected $senha;
+        protected $banco;
+        protected $conexao;
+        protected $qry;
+        protected $dados;
+    }
+
+    public function __construct() {
+        $this->servidor ="localhost";
+        $this->usuario  ="root";
+        $this->senha    ="";
+        $this->banco    ="NomedoBancoAqui";
+        self::conectar();
+    }
+
+    function conectar() {
+        $this->conexao  = @mysqli_connect($this->servidor, $this->usuario, $this->senha) or die ("Não foi possível conectar com o SERVIDOR do banco de dados".mysqli_error());
+        $this->banco    = @mysqli_select_db($this->banco) or die ("Não foi possível conectar ao Banco de Dados".mysqli_error());
+    }
+
+    function executarSQL ($sql) {
+        $this->qry  = @mysqli_query($sql) or die ("Erro ao executar o comando MySQL: $sql <br>".mysqli_error());
+        return $this->qry;
+    }
+
+    function listar ($qr) {
+        $this->dados    = @mysqli_fetch_assoc($qr);
+        return $this->dados;
+    }
+
+
+
+?>
---
+++ classes/manipulaDados.php
@@ -0,0 +1,49 @@
+<?php
+    include_once("conexaoMySQL.php");
+
+    class manipulaDados extends conexaoMySQL {
+        protected $sql;
+        protected $tabela;
+        protected $campos;
+        protected $dados;
+        protected $msg;
+        protected $campoTabela;
+        protected $valorPesquisa;
+
+        public function setTabela ($tbl) {
+            $this->tabela   = $tbl;
+        }
+
+        public function setCampos ($campo) {
+            $this->campos   = $campo;
+        }
+
+        public function setDados ($dado) {
+            $this->dados    = $dado;
+        }
+
+        public function setCampoTabela ($campoTab) {
+            $this->campoTabela  = $campoTab;
+        }
+
+        public function setValorPesquisa ($valorPesq) {
+            $this->valorPesquisa = $valorPesq;
+        }
+
+        public function getMsg () {
+            return $this->msg;
+        }
+
+        public function inserir () {
+            $this->sql  = "INSERT INTO $this->tabela ($this->campos) VALUES ($this->setDados)";
+            self::executarSQL($this->sql);
+        }
+
+        public function excluir () {
+            $this->sql  = "DELETE FROM $this->tabela WHERE $this->campoTabela = '$this->valorPesquisa'";
+            self::executarSQL($this->sql);
+        }
+    }
+
+
+?>
