c4464c119ef54c89daf2ef5c74f4240c0d53084b
Bruno Barreto Murad <bbmurad.dev@gmail.com>
2025-05-06 21:45:45 -0300

    06052025 2145hs
    começo da parte administrativa

---
+++ admin/cabecalho.php
@@ -0,0 +1,13 @@
+<div id="cabecalho-superior">
+    <nav id="menu-superior"></nav>
+
+</div>
+<div id="cabecalho-meio" class="cor1">
+    <h1>HORTIFRUTI ONLINE - O melhor site de compras de frutas e legumes do Rio !</h1>
+    <section class="busca">
+        <h2>SETOR ADMINISTRATIVO</h2>
+    </section>    
+</div>
+<div id="cabecalho-inferior" class="cor2">
+    <nav id="menu-principal"></nav>
+</div>
---
+++ admin/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="estilos/style.css">
+        <title>Hortifruti On Line</title>
+    </head>
+    <body>
+        <div id="principal">
+            <div id="cabecalho">
+                <?php include_once ('cabecalho.php'); ?>
+            </div> <!-- fim da section  cabeçalho-->
+            <div id="corpo-adm">
+                <nav id="lado-esquerdo">
+
+                </nav>
+                <div id="lado-direito">
+
+                </div>
+            </div>
+    </body>
+</html>
